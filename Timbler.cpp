b6d95cbd87eeb42044c52c693c3fa069d129c2a3
foblako <nikitamyaktinov@mail.ru>
2024-07-03 15:59:06 +0300

    Added Bee moving

--- Timbler.cpp
+++ Timbler.cpp
@@ -31,15 +31,63 @@ int main() {
 
 	float beeSpeed = 0.0f;
 
+
+	Texture textureCloud;
+	textureCloud.loadFromFile("graphics/cloud.png");
+
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+
+	spriteCloud1.setPosition(0, 0);
+	spriteCloud2.setPosition(0, 250);
+	spriteCloud3.setPosition(0, 500);
+
+	bool cloud1Active = false;
+	bool cloud2Active = false;
+	bool cloud3Active = false;
+
+	float cloud1Speed = 0.0f;
+	float cloud2Speed = 0.0f;
+	float cloud3Speed = 0.0f;
+
+	Clock clock;
+
 	while (window.isOpen()) {
 		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
 			window.close();
 		}
 
 		window.clear();
+		Time dt = clock.restart();
+
+		if (!beeActive) {
+			srand((int)time(0));
+			beeSpeed = (rand() % 200) + 200;
+
+			srand((int)time(0) * 10);
+			float height = (rand() % 500) + 500;
+			spriteBee.setPosition(2000, height);
+			beeActive = true;
+		}
+		else {
+			spriteBee.setPosition(spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()), spriteBee.getPosition().y);
+
+		}
+		if (spriteBee.getPosition().x < -100) beeActive = false;
 
 		window.draw(spriteBackground);
+
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+		window.draw(spriteCloud3);
+
 		window.draw(spriteTree);
+
+		window.draw(spriteBee);
 
 		window.display();
 
