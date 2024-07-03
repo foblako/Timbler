93eb93c4f72b8784c8d26ebdf3b2148b4e54dee7
foblako <nikitamyaktinov@mail.ru>
2024-07-03 15:18:56 +0300

    Bee and Tree added

--- Timbler.cpp
+++ Timbler.cpp
@@ -6,12 +6,30 @@ int main() {
 	VideoMode vm(1920, 1080);
 	RenderWindow window(vm, "Timber!!", Style::Fullscreen);
 
+
 	Texture textureBackground;
 	textureBackground.loadFromFile("graphics/background.png");
-
 	Sprite spriteBackground;
 	spriteBackground.setTexture(textureBackground);
 	spriteBackground.setPosition(0, 0);
+
+
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810, 0);
+
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(0, 800);
+
+
+	bool beeActive = false;
+
+	float beeSpeed = 0.0f;
 
 	while (window.isOpen()) {
 		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
@@ -21,6 +39,7 @@ int main() {
 		window.clear();
 
 		window.draw(spriteBackground);
+		window.draw(spriteTree);
 
 		window.display();
 
