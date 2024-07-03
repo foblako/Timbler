5ad0c64bc18d7a0d129c012ca940bad0f0db19ad
foblako <nikitamyaktinov@mail.ru>
2024-07-03 08:03:03 +0300

    Background Set

--- Timbler.cpp
+++ Timbler.cpp
@@ -6,12 +6,21 @@ int main() {
 	VideoMode vm(1920, 1080);
 	RenderWindow window(vm, "Timber!!", Style::Fullscreen);
 
+	Texture textureBackground;
+	textureBackground.loadFromFile("graphics/background.png");
+
+	Sprite spriteBackground;
+	spriteBackground.setTexture(textureBackground);
+	spriteBackground.setPosition(0, 0);
+
 	while (window.isOpen()) {
 		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
 			window.close();
 		}
 
 		window.clear();
+
+		window.draw(spriteBackground);
 
 		window.display();
 
